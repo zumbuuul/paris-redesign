44694349ee2279bce26543ebf16909d4aa391a9c
Nenad <nenadstamenkovic04@gmail.com>
2022-10-07 00:37:20 +0200

    dzuda

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js,css}", "*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#0A0908",
+        red: "#49111C",
+        white: "#F2F4F3",
+        lightbrown: "#A9927D",
+        darkbrown: "#5E503F",
+        backgroundColor: "#0B0B0A",
+      },
+      fontFamily: {
+        unna: ["Unna", "serif"],
+        ununtu: ["Ubuntu", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
