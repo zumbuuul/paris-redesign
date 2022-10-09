ff85e0f28faaf976a4356baf0f024db86166c184
zumbuuul <melje11delje@gmail.com>
2022-10-09 16:45:27 +0200

    finished about us

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,css}", "*.{html,js}"],
   theme: {
+    screens: {
+      tablet: "640px",
+      laptop: "1024px",
+      pc: "1280px",
+    },
     extend: {
       colors: {
         black: "#0A0908",
@@ -9,6 +14,8 @@ module.exports = {
         lightbrown: "#A9927D",
         darkbrown: "#5E503F",
         backgroundColor: "#0B0B0A",
+        gradientMid: "#1f1f1f",
+        gradientEnd: "#383838",
       },
       fontFamily: {
         unna: ["Unna", "serif"],
