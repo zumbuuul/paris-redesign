23816ece00d067fca731797396e48ce6d370bdda
Lazar <melje11delje@gmail.com>
2023-01-20 01:05:38 +0100

    added special deal

--- scripts/intersectionObserver.js
+++ scripts/intersectionObserver.js
@@ -7,13 +7,10 @@ const slower = document.querySelectorAll("#sec");
 
 elements = [...slower, ...slow];
 
-console.log(elements);
-
 const options = { treshhold: 1 };
 
 function handleIntersect(entries, observer) {
   entries.forEach((entry) => {
-    console.log(entry.target.id);
     if (entry.isIntersecting && entry.target.id == "prim")
       entry.target.classList.add("slowSlideup");
     if (entry.isIntersecting && entry.target.id == "sec")
